5707f13c6fab9b9b5cd25e647459d398998c339a
Mateusz Ciołkosz <matciolkosz@gmail.com>
2020-05-11 18:39:59 +0200

    New data. Fited polynominal should be at least 3rd level.

--- thermistor-problem.jl
+++ thermistor-problem.jl
@@ -1,11 +1,11 @@
 using Polynomials
-using CSV: file
-using DataFrames: DataFrame!
+using CSV
+using DataFrames
 using Plots
 
 
 
-T_R_raw = file("./data/temp_control_data.csv", 1) |> DataFrame!
+T_R_raw = CSV.file("./data/temp_control_data.csv", 1) |> DataFrames.DataFrame!
 
 data_points = convert(Matrix, T_R_raw[:,1:2])
 
@@ -13,4 +13,8 @@ poly = fit(data_points[:,1], data_points[:,2], 1)
 
 y_lin = map(poly, data_points[:,1])
 
-Plots.plot(data_points[:,1], [data_points[:,2] y_lin])
+poly_tri = fit(data_points[:,1], data_points[:,2], 3) # sufficient polynominal approxymation
+
+y_tri = map(poly_tri, data_points[:,1])
+
+Plots.plot(data_points[:,1], [data_points[:,2] y_lin y_tri])
